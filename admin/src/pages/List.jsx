ec737924feb087bbda5486936b2c59db1461c9f4
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-08 12:25:40 +0530

    Done

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { backendUrl, currency } from '../App'
+import { backendUrl } from '../App'
 import { toast } from 'react-toastify'
 
 const List = ({ token }) => {
@@ -9,15 +9,12 @@ const List = ({ token }) => {
 
   const fetchList = async () => {
     try {
-
       const response = await axios.get(backendUrl + '/api/product/list')
       if (response.data.success) {
         setList(response.data.products.reverse());
-      }
-      else {
+      } else {
         toast.error(response.data.message)
       }
-
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -26,16 +23,13 @@ const List = ({ token }) => {
 
   const removeProduct = async (id) => {
     try {
-
       const response = await axios.post(backendUrl + '/api/product/remove', { id }, { headers: { token } })
-
       if (response.data.success) {
         toast.success(response.data.message)
         await fetchList();
       } else {
         toast.error(response.data.message)
       }
-
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -52,7 +46,6 @@ const List = ({ token }) => {
       <div className='flex flex-col gap-2'>
 
         {/* ------- List Table Title ---------- */}
-
         <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
           <b>Image</b>
           <b>Name</b>
@@ -62,15 +55,14 @@ const List = ({ token }) => {
         </div>
 
         {/* ------ Product List ------ */}
-
         {
           list.map((item, index) => (
             <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
               <img className='w-12' src={item.image[0]} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
-              <p>{currency}{item.price}</p>
-              <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+              <p>Rs {item.price.toLocaleString('en-IN')}</p>
+              <p onClick={() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
             </div>
           ))
         }
